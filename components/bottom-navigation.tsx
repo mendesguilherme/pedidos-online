4b7a5b6cde166924f6fe23d71e89dd319de58d5d
guilhermemendes <geracaoguilherme@gmail.com>
2025-08-05 10:27:14 -0300

    diversos

--- components/bottom-navigation.tsx
+++ components/bottom-navigation.tsx
@@ -18,7 +18,7 @@ export function BottomNavigation() {
   }
 
   return (
-    <div className="bg-primary text-primary-foreground border-t border-accent fixed bottom-0 left-0 right-0 z-50">
+    <div className="bg-primary text-primary-foreground border-t border-accent fixed bottom-[84px] left-0 right-0 z-40">
       <div className="flex justify-around items-center py-2">
         <button
           onClick={() => handleNavigation("/")}
