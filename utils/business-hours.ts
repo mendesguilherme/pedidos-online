31218f3943118370af8165833531cc8b52002642
guilhermemendes <geracaoguilherme@gmail.com>
2025-08-15 10:08:08 -0300

    diversos

--- utils/business-hours.ts
+++ utils/business-hours.ts
@@ -11,7 +11,7 @@ export const BUSINESS_HOURS: BusinessHours = {
   tuesday: { open: "08:00", close: "23:59", isOpen: true },
   wednesday: { open: "00:01", close: "23:59", isOpen: true },
   thursday: { open: "00:01", close: "23:59", isOpen: true },
-  friday: { open: "14:00", close: "23:00", isOpen: true },
+  friday: { open: "07:00", close: "23:00", isOpen: true },
   saturday: { open: "14:00", close: "23:00", isOpen: true },
   sunday: { open: "14:00", close: "23:00", isOpen: true },
 }
